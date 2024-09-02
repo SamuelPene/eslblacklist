ebb99e5a86aec52f3e7b65d6bd29a03cd9fbc5d3
Samuel Pene <samuel.pene@outlook.com>
2024-09-02 21:13:40 +1200

    Styled nav, footer and landing components. Created a results component and styled it.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Landing from "./Components/Landing/landing";
 import Nav from "./Components/Nav/nav";
 import Footer from "./Components/Footer/footer";
+import Results from "./Components/Results/results";
 
 function App() {
   return (
     <div className='App'>
       <Nav />
-      <Landing />
+      {/* <Landing /> */}
+      <Results />
       <Footer />
     </div>
   );
---
+++ client/src/Components/Results/results.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./results.css";
+
+function results() {
+  return (
+    <div className='results'>
+      <div className='results_title'>
+        <h3>Hagwon List</h3>
+        <button>BACK</button>
+      </div>
+      <div className='results_container'></div>
+    </div>
+  );
+}
+
+export default results;
